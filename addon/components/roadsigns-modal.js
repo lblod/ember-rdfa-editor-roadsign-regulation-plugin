cd052b1676ff200ef5104882fa3c1e990b734e83
Niels Vandekeybus <progster@gmail.com>
2021-12-03 11:18:57 +0100

    fix annotations

--- addon/components/roadsigns-modal.js
+++ addon/components/roadsigns-modal.js
@@ -109,12 +109,11 @@ export default class RoadsignRegulationCard extends Component {
       <div property="eli:has_part" prefix="mobiliteit: https://data.vlaanderen.be/ns/mobiliteit#" typeof="besluit:Artikel" resource="http://data.lblod.info/artikels/${uuid()}">
         <div property="eli:number" datatype="xsd:string">Artikel <span class="mark-highlight-manual">nummer</span></div>
         <span style="display:none;" property="eli:language" resource="http://publications.europa.eu/resource/authority/language/NLD" typeof="skos:Concept">&nbsp;</span>
-        <div property="prov:value" datatype="xsd:string">
-          <span class="mark-highlight-manual">Voer inhoud in</span>
-        </div>
-        <div property="mobiliteit:heeftVerkeersmaatregel" typeof="mobiliteit:Mobiliteitsmaatregel" resource="http://data.lblod.info/mobiliteitsmaatregel/${uuid()}">
-          <div property="dct:description">
-            ${html}
+        <div propert="prov:value" datatype="xsd:string">
+          <div property="mobiliteit:heeftVerkeersmaatregel" typeof="mobiliteit:Mobiliteitsmaatregel" resource="http://data.lblod.info/mobiliteitsmaatregel/${uuid()}">
+            <div property="dct:description">
+              ${html}
+            </div>
           </div>
         </div>
       </div>
