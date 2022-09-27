42f0de4bb7d207b3d5a759a6e03262f243c58d6c
abeforgit <arnebertrand@gmail.com>
2022-09-27 22:14:56 +0200

    docs: update obsolete doc comments

--- addon/roadsign-regulation-plugin.js
+++ addon/roadsign-regulation-plugin.js
@@ -1,32 +1,18 @@
 /**
- * Entry point for RoadsignRegulation
- *
- * @module editor-roadsign-regulation-plugin
- * @class RoadSignRegulationPlugin
- * @constructor
- * @extends EmberService
+ * Entrypoint for the roadsign regulation plugin.
  */
 export default class RoadSignRegulationPlugin {
-  /**
-   * Handles the incoming events from the editor dispatcher.  Responsible for generating hint cards.
-   *
-   * @method execute
-   *
-   * @param {string} hrId Unique identifier of the state in the HintsRegistry.  Allows the
-   * HintsRegistry to update absolute selected regions based on what a user has entered in between.
-   * @param {Array} rdfaBlocks Set of logical blobs of content which may have changed.  Each blob is
-   * either has a different semantic meaning, or is logically separated (eg: a separate list item).
-   * @param {Object} hintsRegistry Keeps track of where hints are positioned in the editor.
-   * @param {Object} editor Your public interface through which you can alter the document.
-   *
-   * @public
-   */
   controller;
 
   get name() {
     return 'roadsign-regulation';
   }
 
+  /**
+   * Gets called when the editor loads.
+   * Can optionally be async if needed.
+   * @param controller
+   */
   initialize(controller) {
     this.controller = controller;
     controller.registerWidget({
